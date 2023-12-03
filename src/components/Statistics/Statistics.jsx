3aa2fc634ca063a89b1d746020593510f86e1ff2
AQR88 <timahaua@gmail.com>
2023-12-04 00:00:38 +0200

    className

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,14 @@
+import css from './statistics.module.css';
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return total === 0 ? (
     <h3>There is no statistics</h3>
   ) : (
-    <div>
-      <h3>Good: {good}</h3>
-      <h3>Neutral: {neutral}</h3>
-      <h3>Bad: {bad}</h3>
-      <h3>Total: {total}</h3>
-      <h3>Positive feedbacks: {positivePercentage}%</h3>
+    <div className={css.stat}>
+      <h3 className={css.text}>Good: {good}</h3>
+      <h3 className={css.text}>Neutral: {neutral}</h3>
+      <h3 className={css.text}>Bad: {bad}</h3>
+      <h3 className={css.text}>Total: {total}</h3>
+      <h3 className={css.text}>Positive feedbacks: {positivePercentage}%</h3>
     </div>
   );
 };
